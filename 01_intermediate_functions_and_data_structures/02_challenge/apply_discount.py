3fae1c3ced5bec84ad00ec9bc48225118b9f4831
Seiji Kamiya <10seiji18@gmail.com>
2024-05-17 15:42:00 +0900

    Complete apply_discount function for the challenge

--- 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
+++ 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
@@ -1,2 +1,2 @@
 def apply_discount(products, minimum_price, discount_rate):
-    pass
+    return [(lambda n: (n['name'], n['price']*(1-discount_rate/100)))(product) for product in products if product["price"]>minimum_price]
