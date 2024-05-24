95d0f16eb737aa0cd8dbf59d97f9a84d26951cd4
Seiji Kamiya <10seiji18@gmail.com>
2024-05-24 18:57:51 +0900

    Add library.py for 01_challenge

--- 02_classes_modules_and_packages/01_challenge/library.py
+++ 02_classes_modules_and_packages/01_challenge/library.py
@@ -1,1 +1,15 @@
-# ここにコードを書いてください
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, title, author):
+        self.books.append({
+            "title": title,
+            "author": author,
+        })
+
+    def remove_book(self, title):
+        self.books = [book for book in self.books if book["title"] != title]
+
+    def retrieve_books(self):
+        return self.books
