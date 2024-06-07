4d1c1d3eb3395d7b5171ec5931e9afaa1f85e002
Seiji Kamiya <10seiji18@gmail.com>
2024-06-07 16:48:12 +0900

    Modify mystery_function.py for the challenge

--- 04_error_handling_and_debugging/01_challenge/mystery_function.py
+++ 04_error_handling_and_debugging/01_challenge/mystery_function.py
@@ -1,6 +1,10 @@
 def mystery_function(lst):
     result = lst
     for i in range(len(lst)):
-        if i % 2 == 0:
-            result[i] = i ** 2
+        if result[i] % 2 == 0:
+            result[i] = result[i] ** 2
     return result
+
+if __name__ == "__main__":
+    assert mystery_function([1, 2, 3, 4, 5]) == [1, 4, 3, 16, 5]
+    assert mystery_function([4, 1, 6, 2, 10]) == [16, 1, 36, 4, 100]
